2e8acdee7b154610f6a39a87e731ecb8f11311bd
Jurica <jurica.plesa@gmail.com>
2021-12-09 21:10:30 +0100

    build src init

---
+++ buildSrc/src/main/java/Androidx.kt
@@ -0,0 +1,11 @@
+object Androidx {
+    private const val coreKtxVersion = "1.3.2"
+    const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+
+    private const val appcompatVersion = "1.2.0"
+    const val appcompat = "androidx.appcompat:appcompat:$appcompatVersion"
+
+    private const val constraintLayoutVersion = "2.0.4"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"
+}
